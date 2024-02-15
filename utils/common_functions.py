265e4e9b4c7b3c28c9997ae88eaa115abd98b9a0
Yan-Zhelanov <yzhelanov@gmail.com>
2024-02-15 22:04:56 +0300

    Fix: flake8

--- utils/common_functions.py
+++ utils/common_functions.py
@@ -1,11 +1,14 @@
-import pandas as pd
 from typing import Union
+
+import pandas as pd
 
 
 def read_dataframe_file(path_to_file: str) -> Union[pd.DataFrame, None]:
     if path_to_file.endswith('csv'):
         return pd.read_csv(path_to_file)
-    elif path_to_file.endswith('pickle'):
+    if path_to_file.endswith('pickle'):
         return pd.read_pickle(path_to_file)
-    else:
-        raise ValueError("Unsupported file format. Only '.csv' and '.pickle' formats are supported.")
+    raise ValueError(
+        "Unsupported file format. Only '.csv' and '.pickle' formats are"
+        + ' supported.',
+    )
