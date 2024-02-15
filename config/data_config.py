0926a7d00f43d2596904501591ae37fba71500c8
Yan-Zhelanov <yzhelanov@gmail.com>
2024-02-15 22:34:34 +0300

    Update: ignore typing of EasyDict

--- config/data_config.py
+++ config/data_config.py
@@ -1,7 +1,8 @@
-from easydict import EasyDict
 import os
-from utils.enums import PreprocessingType
 
+from easydict import EasyDict  # type: ignore
+
+from utils.enums import PreprocessingType
 
 ROOT_DIR = os.path.dirname(os.path.dirname(__file__))
 
@@ -12,7 +13,7 @@ data_config.path_to_data = os.path.join(ROOT_DIR, 'data', 'diamonds')
 data_config.type = {
     'train': 'diamonds_train.csv',  # training set
     'valid': 'diamonds_validation.csv',  # valid set
-    'test': 'diamonds_test.csv'  # test set
+    'test': 'diamonds_test.csv',  # test set
 }
 
 data_config.preprocess_type = PreprocessingType.normalization
