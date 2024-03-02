328766db413b67eb120225bc30f450381fca568a
Yan-Zhelanov <yzhelanov@gmail.com>
2024-03-02 20:14:43 +0300

    Update: convert config to a class

--- config/data_config.py
+++ config/data_config.py
@@ -1,19 +1,15 @@
 import os
 
-from easydict import EasyDict  # type: ignore
-
 from utils.enums import PreprocessingType
 
 ROOT_DIR = os.path.dirname(os.path.dirname(__file__))
 
-data_config = EasyDict()
 
-# path to the directory with dataset files
-data_config.path_to_data = os.path.join(ROOT_DIR, 'data', 'diamonds')
-data_config.type = {
-    'train': 'diamonds_train.csv',  # training set
-    'valid': 'diamonds_validation.csv',  # valid set
-    'test': 'diamonds_test.csv',  # test set
-}
-
-data_config.preprocess_type = PreprocessingType.normalization
+class Config(object):
+    PATH_TO_DATA = os.path.join(ROOT_DIR, 'data', 'diamonds')
+    TYPE = {
+        'train': 'diamonds_train.csv',
+        'valid': 'diamonds_validation.csv',
+        'test': 'diamonds_test.csv',
+    }
+    PREPROCESS_TYPE = PreprocessingType.NORMALIZATION
