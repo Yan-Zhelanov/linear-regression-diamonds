91d370da96cfb2dc6af27282c58d5cf8c31bab4c
Yan-Zhelanov <yzhelanov@gmail.com>
2024-03-05 20:52:36 +0300

    Add: dummy model using and checking rmse

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 from config.data_config import Config
 from dataset.dataset import DiamondsDataset
+from model.dummy import Dummy
 from model.linear_regression import LinearRegression
 from utils.enums import SetType
 from utils.metrics import get_rmse
@@ -15,8 +16,11 @@ def main() -> None:
     train_data = dataset(SetType.TRAIN)
     valid_data = dataset(SetType.VALID)
 
+    dummy = Dummy()
+    dummy.fit(train_data['inputs'], train_data['targets'])
+
     lin_reg_model = LinearRegression(
-        number_bases=train_data['inputs'].shape[1] + 1,
+        number_bases=train_data['inputs'].shape[1] + 1, regularization=0.01,
     )
     lin_reg_model.fit(train_data['inputs'], train_data['targets'])
 
@@ -26,8 +30,12 @@ def main() -> None:
     valid_predictions = lin_reg_model(valid_data['inputs'])
     valid_rmse = get_rmse(valid_data['targets'], valid_predictions)
 
+    valid_predictions_dummy = dummy.predict(valid_data['inputs'])
+    valid_rmse_dummy = get_rmse(valid_data['targets'], valid_predictions_dummy)
+
     print(f'RMSE for train: {train_rmse}')
     print(f'RMSE for validation: {valid_rmse}')
+    print(f'RMSE for dummy validation: {valid_rmse_dummy}')
 
     test_data = dataset(SetType.TEST)
     test_predictions = lin_reg_model(test_data['inputs'])
